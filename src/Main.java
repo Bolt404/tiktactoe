f991b7d53d0acb5241c90d5ba3c9f43bb8dcbede
Bolt404 <mads@rknudsen.dk>
2023-12-05 10:12:47 +0100

    gameRound finished, checks if spot already taken.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.InputMismatchException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,40 +7,103 @@ public class Main {
      *TODO Generate the map
      *TODO Place O or X
      *TODO Check who is wining
-     * @param args
      */
-    public static char[][] board = {
+    public static char[][] boarddefault = {
         {'1','2','3'},
         {'4','5','6'},
         {'7','8','9'}
     };
+    public static char[][] boardPlay = new char[boarddefault.length][];
     public static char currentPlayer = 'X';
 
     public static void main(String[] args) {
+        //Need to setup my play board with some values, might as well use the default.
+        for (int i = 0; i < boarddefault.length; i++) {
+            boardPlay[i] = Arrays.copyOf(boarddefault[i], boarddefault[i].length);
+        }
+
         Scanner input = new Scanner(System.in);
+        boolean winnerFound = false;
         System.out.println("WELCOME TO TIKTAKTOE");
-        while(true){
+        while(!winnerFound){
             makeBoard();
             gameRound(input);
+            switchPlayer();
+            //winnerFound = checkIfWinner();
         }
     }
+    public static boolean checkIfWinner(){
+        return true;
+    }
     public static void makeBoard() {
-        System.out.printf("+---+---+---+%n");
+        //Header of my board.
+        System.out.printf("%sCURRENT PLAY   %sSELECTION BOARD%s%n",YELLOW,RED,RESET);
+        System.out.printf("+---+---+---+  +---+---+---+%n");
+
         for (int i = 0; i < 3; i++) {
-            System.out.printf("| ");
+            System.out.print("| ");
+
+            // Print elements from the first array (boardPlay) this is what the player changes.
             for (int j = 0; j < 3; j++) {
-                System.out.printf("%s%c%s%s",YELLOW, board[i][j], RESET, " | ");
+                System.out.printf("%s%c%s%s", YELLOW, boardPlay[i][j], RESET, " | ");
             }
-            System.out.printf("%n+---+---+---+%n");
+
+            // Print a separator
+            System.out.print(" | ");
+
+            // Print elements from the second array (boarddefault) this is to reference what number belongs where.
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%s%c%s%s", RED, boarddefault[i][j], RESET, " | ");
+            }
+            //Footer of my board.
+            System.out.printf("%n+---+---+---+  +---+---+---+%n");
         }
+
     }
     public static void gameRound (Scanner input){
         System.out.printf("%sYour turn player %s%c%s%n", GREEN, YELLOW, currentPlayer, RESET);
-        System.out.printf("Please input your desired square: ");
+        System.out.print("Please input your desired square: ");
         int number = checkInput(input, 1, 9);
+
+        switch (number){
+            case 1:
+                updateBoard(0,0);
+                break;
+            case 2:
+                updateBoard(0,1);
+                break;
+            case 3:
+                updateBoard(0,2);
+                break;
+            case 4:
+                updateBoard(1,0);
+                break;
+            case 5:
+                updateBoard(1,1);
+                break;
+            case 6:
+                updateBoard(1,2);
+                break;
+            case 7:
+                updateBoard(2,0);
+                break;
+            case 8:
+                updateBoard(2,1);
+                break;
+            case 9:
+                updateBoard(2,2);
+                break;
+        }
+    }
+    public static void updateBoard (int row, int column){
+        if (boardPlay[row][column] == 'X' || boardPlay[row][column] == 'O'){
+            System.out.printf("%s%S%s%n",RED,"Already taken, please try again.",RESET);
+        } else {
+            boardPlay[row][column] = currentPlayer;
+        }
+
     }
     public static int checkInput(Scanner input, int min, int max) {
-
         while (true) {
             if (input.hasNextInt()) {
                 int number = input.nextInt();
@@ -54,6 +117,13 @@ public class Main {
                 System.out.printf("%s%S [%d, %d].%s%n", RED, "Invalid input. Please enter a number within the range", min, max, RESET);
                 input.next();
             }
+        }
+    }
+    public static void switchPlayer (){
+        if (currentPlayer == 'X') {
+            currentPlayer = 'O';
+        } else {
+            currentPlayer = 'X';
         }
     }
     public static final String RESET = "\u001B[0m";
